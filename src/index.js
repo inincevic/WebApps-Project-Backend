317e8986db55c5c22eca0792ee7f1963ea6fbd83
inincevic <inincevic@unipu.hr>
2022-05-24 11:39:35 +0200

    trying to establish communication with frontend

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const port=3000;
+const port = 3000;
 
-app.get('/',(req,res)=>{
-    console.log("Izvrsava se!")
-    res.send('Hello world!');
-})
-app.listen(port,()=>{
-    console.log('Example app listening on port', port)
+//razumijevanje jsona
+app.use(express.json());
+app.use(cors());
+
+app.get("/pokemoni", (req, res) => {
+  console.log("Izvrsava se!");
+  let pokemoni = [
+    {
+      name: "Trubbish",
+      primary_type: "Poison",
+      secondary_type: "No secondary type",
+      primary_colour: "Green",
+      secondary_colour: "No secondary colour",
+      stage: "Base",
+      evolution_method: "This pokemon is a base stage"
+    },
+    {
+        name: "Pikachu",
+        primary_type: "Electric",
+        secondary_type: "No secondary type",
+        primary_colour: "Yellow",
+        secondary_colour: "No secondary colour",
+        stage: "Base",
+        evolution_method: "Friendship"
+    }
+  ];
+  res.send(pokemoni);
+});
+app.listen(port, () => {
+  console.log("Example app listening on port", port);
 });
