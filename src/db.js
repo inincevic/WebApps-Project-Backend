783acf953e0c93ab1d27475037969c4199de1595
inincevic <inincevic@unipu.hr>
2022-06-22 17:09:39 +0200

    enabled finding in mongo

--- src/db.js
+++ src/db.js
@@ -1,35 +1,24 @@
 import mongo from "mongodb";
 
-let connection_string = "mongodb+srv://inincevic:Umbreon1@cluster0.lwuw4.mongodb.net/test";
+let connection_string =
+  "mongodb+srv://inincevic:Umbreon1@cluster0.lwuw4.mongodb.net/test";
 
 let client = new mongo.MongoClient(connection_string, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
- });
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
- let db = null;
-
-//  client.connect((err) => {
-//      if (err) {
-//          console.error(err);
-//          return;
-//      }
-//      console. log ("Database connected successfully!");
-//
-//      client. close();
-// });
-
+let db = null;
 
 function isConnected() {
-    return !!client && !!client.topology && client.topology.isConnected();
+  return !!client && !!client.topology && client.topology.isConnected();
 }
 // eksportamo Promise koji resolva na konekciju
 export default async () => {
-     if (!db || !isConnected ()) {
-         await client.connect()
-         db = client.db("PokeGuesserProject");
-         console.log("Connected OK")
-     }
-     else console.log("Already Connected");
-     return db;
-}
+  if (!db || !isConnected()) {
+    await client.connect();
+    db = client.db("PokeGuesserProject");
+    console.log("Connected OK");
+  } else console.log("Already Connected");
+  return db;
+};
